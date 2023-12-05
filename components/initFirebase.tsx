97fa5d68825d1ef3fdb5a2860647e503ec1b66f8
edxen <edabogajr@gmail.com>
2023-12-05 12:04:44 +0800

    test 2 vercel deployment

--- components/initFirebase.tsx
+++ components/initFirebase.tsx
@@ -1,20 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 let firebaseConfig = {};
-try {
-    firebaseConfig = require('@/.env/firebase.json');
-} catch (error) {
-    if (process.env.FIREBASE_API_KEY && process.env.FIREBASE_AUTH_DOMAIN && process.env.FIREBASE_PROJECT_ID && process.env.FIREBASE_STORAGE_BUCKET) {
-        firebaseConfig = {
-            apiKey: process.env.FIREBASE_API_KEY,
-            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            storageBucket: process.env.FIREBASE_STORAGE_BUCKET
-        };
-    } else {
-        console.log(`no environment variables set`);
-    }
-}
+firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+};
 
 const app = initializeApp(firebaseConfig);
 

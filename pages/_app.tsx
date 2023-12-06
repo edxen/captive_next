07b5dbe81a0bf171d77e076647d3a259a0e20ca8
edxen <edabogajr@gmail.com>
2023-12-06 20:14:00 +0800

    removed initialProps declaration in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,7 @@ import Layout from '@/components/layout';
 import { SiteProvider } from '@/components/context';
 import { useState, useEffect } from 'react';
 
-interface MyAppProps extends AppProps {
-  styles: string;
-}
-
-const MyApp = ({ Component, pageProps, styles }: MyAppProps) => {
+const App = ({ Component, pageProps }: AppProps) => {
   const [isMounted, setMount] = useState(false);
 
   useEffect(() => {
@@ -32,17 +28,4 @@ const MyApp = ({ Component, pageProps, styles }: MyAppProps) => {
   );
 };
 
-MyApp.getInitialProps = async ({ Component, ctx }: { Component: any; ctx: any; }) => {
-  const sheet = new ServerStyleSheet();
-  try {
-    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
-    const page = sheet.collectStyles(<Component {...pageProps} />);
-    const styleTags = sheet.getStyleTags();
-
-    return { ...pageProps, styles: styleTags };
-  } finally {
-    sheet.seal();
-  }
-};
-
-export default MyApp;
+export default App;
